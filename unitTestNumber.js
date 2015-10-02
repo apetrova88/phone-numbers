b164162221c0eb9bb32de59e1bac46cb1fd1d9d1
valdikadf <Khvorostaniy@gmail.com>
2015-10-02 20:47:01 +0300

    Unit test 1.2

--- unitTestNumber.js
+++ unitTestNumber.js
@@ -1,3 +1,15 @@
 /**
  * Created by IT PROSTIR on 29.09.2015.
  */
+QUnit.test( "Phone Numbers", function( ) {
+    equal( detectOperator(80931111111),"Life:)","Passed! it's Life:)" );
+    equal( detectOperator(80961111111),"Kyivstar","Passed! it's Kyivstar" );
+    equal( detectOperator(80671111111),"Kyivstar","Passed! it's Kyivstar" );
+    equal( detectOperator(80661111111),"MTS","Passed! it's MTS" );
+    equal( detectOperator(80631111111),"Life:)","Passed! it's Life:)" );
+    equal( detectOperator(80391111111),"Golden Telecom","Passed! it's Golden Telecom" );
+    equal( detectOperator(80981111111),"Kyivstar","Passed! it's Kyivstar" );
+    equal( detectOperator(80991111111),"MTS","Passed! it's MTS" );
+    equal( detectOperator(80681111111),"Beeline","Passed! it's Beeline" );
+    equal( detectOperator(""),"no info","Passed! it's no info" );
+});
