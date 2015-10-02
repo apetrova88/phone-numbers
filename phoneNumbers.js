76c93c04cbea0c92e4ca1afe80b1911e55d92528
dtv <ann.petrova.88@gmail.com>
2015-10-02 10:44:32 +0300

    function detectOperator was added

--- phoneNumbers.js
+++ phoneNumbers.js
@@ -1,5 +1,11 @@
-function oper(nomer){
-
-    var oper = nomer;
-    return oper;
+function detectOperator(a){
+    a = 80931111111;
+    console.log(String(a).substr(1, 3));
+    var op="no info";
+    if (String(a).substr(1,3) === "039") return "Golden Telecom";
+    if (String(a).substr(1,3) === "063" || String(a).substr(1,3) === "093") return "Life:)";
+    if (String(a).substr(1,3) === "066" || String(a).substr(1,3) === "095" || String(a).substr(1,3) === "099") return "MTS";
+    if (String(a).substr(1,3) === "067" || String(a).substr(1,3) === "096" || String(a).substr(1,3) === "097" || String(a).substr(1,3) === "098") return "Kyivstar";
+    if (String(a).substr(1,3) === "068") return "Beeline";
+    else return op;
 }
